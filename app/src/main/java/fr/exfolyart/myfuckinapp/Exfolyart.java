525cde6593967284990412d7a938004911f7f98e
Rewan HAMNOUCHE <rewan.hamnouche@gmail.com>
2021-01-02 23:35:37 +0100

    2nd : 3 activities

--- app/src/main/java/fr/exfolyart/myfuckinapp/Exfolyart.java
+++ app/src/main/java/fr/exfolyart/myfuckinapp/Exfolyart.java
@@ -2,22 +2,27 @@ package fr.exfolyart.myfuckinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Exfolyart extends AppCompatActivity {
-
     private TextView points;
     private ImageView exfolyart;
     private int clicks = 0;
+    private Button goto3;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cookie);
 
+        goto3 = (Button) findViewById(R.id.goto3);
         this.points = (TextView) findViewById(R.id.points);
         this.exfolyart = (ImageView) findViewById(R.id.exfolyart);
 
@@ -28,5 +33,17 @@ public class Exfolyart extends AppCompatActivity {
                 points.setText("Points : " + clicks);
             }
         });
+
+        goto3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {openActivity3();}});
+    }
+    public void openActivity3()
+    {
+        Intent goto3 = new Intent(this, MainActivity2.class);
+        startActivity(goto3);
+        finish();
     }
 }
---
+++ app/src/main/java/fr/exfolyart/myfuckinapp/MainActivity2.java
@@ -0,0 +1,17 @@
+package fr.exfolyart.myfuckinapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Exfolyart on 02/01/2021.
+ */
+public class MainActivity2 extends AppCompatActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
